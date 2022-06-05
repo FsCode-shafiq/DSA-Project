e7936ff16e87388a8cfd5cb572527d42d923d90e
Shafiq Ur Rehman <69121735+FsCode-shafiq@users.noreply.github.com>
2022-06-06 01:59:31 +0500

    last commit

--- src/stackController.java
+++ src/stackController.java
@@ -222,4 +222,14 @@ public class stackController {
 
     }
 
+    @FXML
+    void Back(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("mainScene.fxml"));
+        stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
